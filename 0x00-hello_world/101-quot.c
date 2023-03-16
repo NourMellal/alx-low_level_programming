112573e7b766f3b6b3a63e6ea1d98a4d6ee74069
NourMellal <mellal.nour9@gmail.com>
2023-03-16 16:47:05 -0700

    6-size.c

---
+++ 0x00-hello_world/101-quot.c
@@ -0,0 +1,18 @@
+/*
+ * File: 101-quote.c
+ * Auth: moamineayadi
+ */
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 1 (Failure)
+ */
+int main(void)
+{
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
+	return (1);
+}
