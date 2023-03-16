20389c8189d374ec8870a98b1761ad845c7e3355
NourMellal <mellal.nour9@gmail.com>
2023-03-16 16:37:34 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %zu byte(s)\n"(d));
-printf("Size of a float: %zu byte(s)\n"(f));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+printf("Size of a float: %zu byte(s)\n", sizeof(f));
 
-return(0);
+return (0);
 }
 
