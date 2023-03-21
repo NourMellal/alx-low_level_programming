cd062b2b91966e22176f0580c5ff822c5428d649
NourMellal <mellal.nour9@gmail.com>
2023-03-21 20:04:53 +0000

    3rd commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+ void print_alphabet_x10(void)
+{
+	char line;
+	for(line = 0; line <= 9; line++)
+	{
+		char letter = 'a';
+		while(letter != 'z'+1)
+		{
+			_putchar (letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
