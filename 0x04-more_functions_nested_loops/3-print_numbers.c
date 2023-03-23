0833584ccd210bc3a2d1e9dc92b0a55f1f714620
NourMellal <mellal.nour9@gmail.com>
2023-03-23 16:10:11 +0000

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	for (c >= '0'; c <= '9'; c++)
+		_putchar ('c');
+}
+	_putchar ('\n');
