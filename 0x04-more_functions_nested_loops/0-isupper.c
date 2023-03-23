7ff0e10c2f47ddf2212535eb2c9b0154392ae808
NourMellal <mellal.nour9@gmail.com>
2023-03-23 15:04:47 +0000

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _putchar - functionc parametr
+ *@c :parameter
+ * Return: Always 0.
+ */
+
+ int _putchar(char c)
+ {
+	if (int c >= 'A' && int c <= 'Z')
+	{
+		return (1);
+		else
+			return (0);
+	}
+ }
