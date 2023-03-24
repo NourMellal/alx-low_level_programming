ea1fc71895c41ce3db8e34f8e9df730e7e0760f8
NourMellal <mellal.nour9@gmail.com>
2023-03-24 00:27:33 +0000

    update

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,16 @@ void print_square(int size)
 {
 int i, j;
 
+if (n <= 0)
+{
+	_putchar('\n');
+}
 for (i = 0; i < size; i++)
 {
 	for (j = 0; j < size; j++)
 	{
 		_putchar('#');
 	}
-}
 	_putchar('\n');
+}
 }
