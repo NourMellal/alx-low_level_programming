594514f83b55d1fcfa39ae86174f03d979dc253e
NourMellal <mellal.nour9@gmail.com>
2023-03-27 15:54:50 +0000

    update

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,15 +6,15 @@
  *
  * Return: the length of the string
  */
- int _strlen(char *s)
+int _strlen(char *s)
 {
 	int i;
-	
+
 	i = 0;
-	
 	while (s[i] != '\0')
 	{
-	i++;
-	return (i);
+		i++;
 	}
-}	
+
+	return (i);
+}
