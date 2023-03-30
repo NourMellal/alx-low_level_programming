ba5046fea7100e446380e3060e04b095e895c408
NourMellal <mellal.nour9@gmail.com>
2023-03-30 00:38:41 +0000

    update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string -  function that capitalizes all words of a string.
+ * @s : the string
+ * Return: uppercase string
+*/
+
+char *cap_string(char *s);
+{
+	int i, j;
+
+	char spe[13] = {',', ';', '.', '!', '?', '"',
+	'(', ')', '{',  '}', ' ', '\t', '\n'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
