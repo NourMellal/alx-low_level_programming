2eeca41803170513c9deb3cda396d332d3c8eb25
NourMellal <mellal.nour9@gmail.com>
2023-03-30 02:41:25 +0000

    update

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,14 @@ char *rot13(char *s)
 {
 	int i, j;
 
-	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; a[j] != '\0'; j++)
 		{
-			if s(s[i] == a[j])
+			if (s[i] == a[j])
 			{
 				s[i] = b[j];
 				break;
