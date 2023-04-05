1e218851a623b73315145701c7e8d066bb7b50cf
NourMellal <mellal.nour9@gmail.com>
2023-04-05 22:03:26 +0000

    says if an integer is a prime number or not

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int prime(int n, int i);
+int actual_prime(int n, int i);
 /**
 * is_prime_number - says if an integer is a prime number or not
 * @n: number to
@@ -13,14 +13,22 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (prime(n, n - 1));
+	return (actual_prime(n, n - 1));
 }
 
-int is_prime_number(int n)
+/**
+* actual_prime - calculate if a number is prime recursively
+* @n: number to evaluate
+* @i: iterator
+*
+* Return: 1 if n is prime, 0 if not
+*/
+
+int actual_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (prime(n, i - 1));
+	return (actual_prime(n, i - 1));
 }
