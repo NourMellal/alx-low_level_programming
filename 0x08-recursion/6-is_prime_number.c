28a858f9c974410d1f1d7f540967e720378fd635
NourMellal <mellal.nour9@gmail.com>
2023-04-05 23:52:27 +0000

    actual_prime - calculate

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,9 +11,7 @@ int actual_prime(int n, int i);
 
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (actual_prime(n, n - 1));
+	return (actual_prime(n, 1));
 }
 
 /**
@@ -26,9 +24,13 @@ int is_prime_number(int n)
 
 int actual_prime(int n, int i)
 {
+	if (n <= 1)
+		return (0);
 	if (i == 1)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (actual_prime(n, i - 1));
+	if (i == n / 2)
+		return (1);
+	return (actual_prime(n, i + 1));
 }
