81118f48716d856c859b9bc356b9e69f22d03470
NourMellal <mellal.nour9@gmail.com>
2023-04-13 09:43:31 +0000

    seconde commit for today

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * malloc_checked - Allocates memory using malloc.
  * @b: The number of bytes to allocate.
  *
  * Return: A pointer to the allocated memory.
- *         If malloc fails, the function terminates the program with exit code 98.
+ * If malloc fails, the function terminates the program with exit code 98.
  */
 void *malloc_checked(unsigned int b)
 {
