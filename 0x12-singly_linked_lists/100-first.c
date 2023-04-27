7b3f46795d4cd46aa3802ff4f76f3c504b6b2250
NourMellal <mellal.nour9@gmail.com>
2023-04-27 19:18:28 +0000

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_message(void) __attribute__((constructor));
+
+/**
+ * print_message - Prints a message before the main function is executed.
+ *
+ * Return: void.
+ */
+void print_message(void)
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
