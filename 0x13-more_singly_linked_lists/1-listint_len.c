7f79ea23d3e7775d49d4234d829f85f9dd356f3c
NourMellal <mellal.nour9@gmail.com>
2023-04-29 15:44:48 +0000

    returns the number of elements in a listint_t list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a listint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}
