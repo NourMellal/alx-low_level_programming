c03e196f8e57b0dff79cae361c7a09434751d31b
NourMellal <mellal.nour9@gmail.com>
2023-05-08 16:01:52 +0100

    Reads a text file and writes its contents to standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text file and writes its contents to standard output.
+ * @filename: The name of the file to read.
+ * @letters: The maximum number of letters to read and write.
+ *
+ * Return: The actual number of letters read and written, or 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
