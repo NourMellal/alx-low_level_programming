27aee8a6128004cfe17dacd04ae65bcef61f2062
NourMellal <mellal.nour9@gmail.com>
2023-05-08 16:03:01 +0100

    Reads a text file and writes its contents to standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and writes its contents to standard output.
+ * read_textfile - Reads a text file and writes
+ * its contents to standard output.
  * @filename: The name of the file to read.
  * @letters: The maximum number of letters to read and write.
  *
