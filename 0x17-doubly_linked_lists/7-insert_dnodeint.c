6dc63a080735dcaa61c5ec748e77af1cbb9f54db
NourMellal <mellal.nour9@gmail.com>
2023-06-15 19:29:10 +0100

    fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for ( ;idx != 1; idx--)
+	for ( ; idx != 1; idx--)
 	{
 		curr = curr->next;
 		if (curr == NULL)
