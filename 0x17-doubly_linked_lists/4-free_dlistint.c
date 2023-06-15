f31c96f02c34a14a298ea3109cac60e1cf6e3c87
NourMellal <mellal.nour9@gmail.com>
2023-06-15 16:20:47 +0100

    betty fix!

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - Frees a doubly linked list
+ * @head: Pointer to the head of the list
+ *
+ * Return: None
+ */
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *tmp;
