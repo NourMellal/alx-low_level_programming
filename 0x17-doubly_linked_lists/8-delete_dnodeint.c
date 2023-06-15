cd473fd7c43229fc13bdc620fc075fd913f0be7a
NourMellal <mellal.nour9@gmail.com>
2023-06-15 20:43:02 +0100

    8-delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -32,11 +32,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	else
 	{
-		tmp->next->prev = tmp->next;
+		tmp->prev->next = tmp->next;
 		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
 	}
 	free(tmp);
-
 	return (1);
 }
