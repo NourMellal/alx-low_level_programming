a4f89a8295e1b5b3307063a02b2bdea1af0ce73f
NourMellal <mellal.nour9@gmail.com>
2023-07-09 19:16:00 +0100

    fix unused variable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,27 @@
 #include "hash_tables.h"
 
 /**
+ * check_collision_in_node - check for collision in the hole list
+ * @head: head of the linked list
+ * @key: hash table key
+ * Return: 1 for success, 0 for failure
+ *
+*/
+hash_node_t *check_collision_in_node(hash_node_t *head, const char *key)
+{
+	hash_node_t *tmp = head;
+
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp);
+
+		tmp = tmp->next;
+	}
+	return (tmp);
+}
+
+/**
 * hash_table_set - Description: The function hash_table_set adds or
 * updates the value of a key in a hash table.
 * Parameters:
@@ -11,7 +32,7 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
+	hash_node_t *new, *tmp;
 	unsigned long int idx, size;
 
 	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
@@ -19,11 +40,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	size = ht->size;
 	idx = key_index((const unsigned char *)key, size);
+	tmp = check_collision_in_node(ht->array[idx], key);
 
-
-	if (ht->array[idx] != NULL && strcmp(ht->array[idx]->key, key) == 0)
+	if (tmp)
 	{
-		ht->array[idx]->value = strdup(value);
+		tmp->value = strdup(value);
 		return (1);
 	}
 	new = malloc(sizeof(hash_node_t));
