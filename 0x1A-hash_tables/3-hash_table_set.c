af90b2cb5b8d1b244c05540044bca7b598087fa5
NourMellal <mellal.nour9@gmail.com>
2023-07-09 18:50:57 +0100

    fix unused variable

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if(ht->array[idx] != NULL || strcmp(ht->array[idx]->key, key) == 0)
 	{
-		free (ht->array[idx]->value);
 		ht->array[idx]->value = strdup(value);
 		return (1);
 	}
