acd4582595ecaec4e2aeb11bff8825555f37f664
NourMellal <mellal.nour9@gmail.com>
2023-07-09 03:24:14 +0100

    0-hash_table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_table_t *) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (ht->array == NULL)
 		return (NULL);
