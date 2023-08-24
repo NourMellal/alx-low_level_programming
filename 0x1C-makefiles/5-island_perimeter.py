e6c900d744488c78cb8dd4fbd0363fdb6a560dc2
NourMellal <mellal.nour9@gmail.com>
2023-08-24 01:43:13 +0100

    count perim

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def island_perimeter(grid):
+    """
+        Finde perimeter in an island
+    """
+    v = set()
+
+    def dfs(i, j):
+        """
+        dfs algorithm to find the perimitre and stores the visited island
+        """
+        if i >= len(grid) or j >= len(grid[0]) or \
+           i < 0 or j < 0 or grid[i][j] == 0:
+            return 1
+
+        if (i, j) in v:
+            return 0
+
+        v.add((i, j))
+        perm = dfs(i, j + 1)
+        perm += dfs(i + 1, j)
+        perm += dfs(i, j - 1)
+        perm += dfs(i - 1, j)
+        return perm
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j]:
+                return (dfs(i, j))
